7fea94e15f536e3e3325a364846d5dd4278e7c1c
Thomas <Thomas@192.168.1.54>
2020-11-23 22:30:48 +0100

    Ajout d'autres commandes: #cote nom_de_l'équipe --> permet de connaitre la cote #bet nom_de_l'équipe valeur --> permet de parierune valeur sur une equipe (incomplet)

--- src/discordBot/Commands.java
+++ src/discordBot/Commands.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 public class Commands extends ListenerAdapter{
 	public void onGuildMessageReceived(GuildMessageReceivedEvent event)
 	{
+		//Pour lire les arguments saisies sur Discord
 		String[] args = event.getMessage().getContentRaw().split("\\s+");
 		
 		if(args[0].equalsIgnoreCase(Main.prefix + "hello"))
@@ -13,6 +14,74 @@ public class Commands extends ListenerAdapter{
 			event.getChannel().sendTyping().queue();
 			event.getChannel().sendMessage("Hello everybody :)").queue();
 		}
+/*		
+		if(args[1].equalsIgnoreCase("cote"))
+		{
+			event.getChannel().sendTyping().queue();
+			event.getChannel().sendMessage("Cote à ").queue();
+		}
+			
+		if(args[2].equalsIgnoreCase("A"))
+		{
+			event.getChannel().sendTyping().queue();
+			event.getChannel().sendMessage("" + Main.getA()+ " pour l'équipe A.").queue();
+		}
+*/		
+		/*	
+		if(args[1].equalsIgnoreCase("cote") && args[2].equalsIgnoreCase("B"))
+		{
+			event.getChannel().sendTyping().queue();
+			event.getChannel().sendMessage("Cote à " + Main.getB()+ " pour l'équipe B.").queue();
+		}
+		
+		if(args[1].equalsIgnoreCase("bet") && args[2].equalsIgnoreCase(""))
+		{
+			event.getChannel().sendTyping().queue();
+			event.getChannel().sendMessage("Cote à " + Main.getB()+ " pour l'équipe B.").queue();
+		}
+		*/
+		
+		
+		if(args[0].equalsIgnoreCase(Main.prefix + "cote") && args[1].equalsIgnoreCase("A"))
+		{
+			event.getChannel().sendTyping().queue();
+			event.getChannel().sendMessage("Cote à " + Main.getA()+ " pour l'équipe A.").queue();
+		}
+		
+		if(args[0].equalsIgnoreCase(Main.prefix + "cote") && args[1].equalsIgnoreCase("B"))
+		{
+			event.getChannel().sendTyping().queue();
+			event.getChannel().sendMessage("Cote à " + Main.getB()+ " pour l'équipe B.").queue();
+		}
+	/*	
+		if(args[0].equalsIgnoreCase(Main.prefix + "bet") && args[1].equalsIgnoreCase("A"))
+		{
+			if(args[2].isEmpty() == true) //ca n'a pas fonctionné
+			{
+				event.getChannel().sendMessage("Veuillez réassayer en entrant la somme que vous voulez parier").queue();
+			}
+			else
+			{
+				event.getChannel().sendTyping().queue();
+				event.getChannel().sendMessage("Paris validé.").queue();
+				//faire une action
+			}
+			
+		}
+	 */
+		
+		if(args[0].equalsIgnoreCase(Main.prefix + "bet") && args[1].equalsIgnoreCase("A") && args[2].isEmpty() == false)
+		{
+			event.getChannel().sendTyping().queue();
+			event.getChannel().sendMessage("Paris validé.").queue();
+			//faire une action
+		}
+		else if(args[0].equalsIgnoreCase(Main.prefix + "bet") && args[1].equalsIgnoreCase("A") && args[2].equalsIgnoreCase(""))
+		{
+			//réaliser une gestion d'erreur, surment avec try catch, throw ?
+			event.getChannel().sendMessage("Veuillez réassayer en entrant la somme que vous voulez parier").queue();
+		}
+		
 	}
 	
 }
--- src/discordBot/Main.java
+++ src/discordBot/Main.java
@@ -2,25 +2,41 @@ package discordBot;
 
 import javax.security.auth.login.LoginException;
 
-import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
 
 public class Main {
+	//creation du jda
 	public static JDA jda;
 	
+	//prefix à utiliser pour que le bot reconnaisse qu'on lui parle
 	public static String prefix = "#";
 	
-	public static void main(String[] args) throws LoginException
+	public static int getA() 
 	{
-		/*
-		jda = new JDABuilder(AccountType.BOT).setToken("NzgwMzgyMjMxNDExNjg3NDQ1.X7uRlg.Yc-yiu67ZbqSCN8Rcf7VIqG-CTQ").build();
-		jda.getPresence().setStatus(OnlineStatus.IDLE);
-		*/
-		
-		jda = JDABuilder.createDefault("NzgwMzgyMjMxNDExNjg3NDQ1.X7uRlg.Yc-yiu67ZbqSCN8Rcf7VIqG-CTQ").build();
+		return 2; //cote_equipe_A
+	}
+	public static int getB() 
+	{
+		return 5; //cote_equipe_B
+	}
+	
+
+	
+	public static void main(String[] args) throws LoginException, InterruptedException
+	{
+		//crée le bot avec le token adapté		
+		jda = JDABuilder.createDefault("NzgwMzgyMjMxNDExNjg3NDQ1.X7uRlg.Yc-yiu67ZbqSCN8Rcf7VIqG-CTQ")
+				.setActivity(Activity.watching("Ready to take bet"))
+				.build()
+				.awaitReady(); //This method will block until JDA has reached the status JDA.Status.CONNECTED.
+
+		//appel de commande
 		jda.addEventListener(new Commands());
+		jda.getPresence().setStatus(OnlineStatus.IDLE);
+	
 	
 	}
 }
