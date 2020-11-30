b4b3bf4e4c4c9349025c748048456601aefb84dc
Thomas <Thomas@DESKTOP-2G2411S>
2020-11-30 16:16:17 +0100

    - Ajout de la commande info
    - #info permet de connaitre la liste des commandes.
    
    - Utilisation d'un embed pour l'affichage de #info

--- src/discordBot/Commands.java
+++ src/discordBot/Commands.java
@@ -1,24 +1,44 @@
 package discordBot;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class Commands extends ListenerAdapter{
 	public void onGuildMessageReceived(GuildMessageReceivedEvent event)
 	{
+
 		
 		// to read arguments type on discord
 		String[] args = event.getMessage().getContentRaw().split("\\s+");
 		
-		
+/*--------------------------------------------------------------------------------------------*/		
 		// type #hello to say hello to the bot
 		if(args[0].equalsIgnoreCase(Main.prefix + "hello"))
 		{
 			event.getChannel().sendTyping().queue();
 			event.getChannel().sendMessage("Hello everybody :)").queue();
 		}
-
 		
+/*--------------------------------------------------------------------------------------------*/		
+		// type #info to display all commands
+		if(args[0].equalsIgnoreCase(Main.prefix + "info"))
+		{
+			EmbedBuilder info = new EmbedBuilder();
+			info.setTitle("Liste des commandes :");
+			info.addField("Pour connaitre une cote :", "#cote nom_de_l'équipe", false);
+			info.addField("Pour faire un paris :", "#bet nom_de_l'équipe somme_engagée", false);
+			info.setColor(0x9003fc);
+			
+			event.getChannel().sendTyping().queue();
+			event.getChannel().sendMessage(info.build()).queue();
+			info.clear(); //Resets this builder to default state.
+
+			
+		}
+
+	
+/*--------------------------------------------------------------------------------------------*/
 		// allow to know the odds of betting of the team you that you want
 		// improvement to do with the team selection
 		if(args[0].equalsIgnoreCase(Main.prefix + "cote") && args[1].equalsIgnoreCase("A"))
@@ -33,7 +53,8 @@ public class Commands extends ListenerAdapter{
 			event.getChannel().sendMessage("Cote à " + Main.getB()+ " pour l'équipe B.").queue();
 		}
 		
-	
+
+/*--------------------------------------------------------------------------------------------*/		
 		//allow to bet on the selected team
 		if(args[0].equalsIgnoreCase(Main.prefix + "bet"))
 		{
@@ -41,7 +62,7 @@ public class Commands extends ListenerAdapter{
 		    if(args.length == 2)
 		    {
 		        event.getChannel().sendMessage("Veuillez réassayer en verifiant si vous "
-		        		+ "avez bien saisi l'équipe et le montant à parier").queue();
+		        		+ "avez bien saisi l'équipe et le montant à parier (voir #info)").queue();
 		        //System.exit(0);
 		    }
 		    if(args.length == 3)
@@ -55,6 +76,8 @@ public class Commands extends ListenerAdapter{
 		    }
 		    
 		}
+
+/*--------------------------------------------------------------------------------------------*/
 
 	
 	}
