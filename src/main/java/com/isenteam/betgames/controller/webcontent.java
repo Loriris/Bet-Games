6d96e01ffd6883ff6c1c1af8b7f14ddb4fa591ce
Loriris <lorismartinez1202@gmail.com>
2021-04-16 10:14:23 +0200

    Amélioration site web Première approche SecretManager pour le token API de discord, à fixer. Ajout librairie et dépendances google

--- src/main/java/com/isenteam/betgames/controller/webcontent.java
+++ src/main/java/com/isenteam/betgames/controller/webcontent.java
@@ -1,13 +1,16 @@
 package com.isenteam.betgames.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class webcontent {
 	
-	@RequestMapping(value = "/index")
-	public String index() {
+	@RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+	public String index(Model model) {
+
 		return "index";
 	}
 
