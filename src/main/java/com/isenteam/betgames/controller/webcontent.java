2d75ca2ffa328697cfb8cdebd7fd4405f7525c9f
Loriris <lorismartinez1202@gmail.com>
2021-04-27 15:54:44 +0200

    Amélioration graphique site web Préparation de la pages fonctionnalités

--- src/main/java/com/isenteam/betgames/controller/webcontent.java
+++ src/main/java/com/isenteam/betgames/controller/webcontent.java
@@ -1,17 +1,19 @@
 package com.isenteam.betgames.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class webcontent {
 	
-	@RequestMapping(value = "/index" )
+	@RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
 	public String index() {
-
 		return "index";
 	}
 
+	@RequestMapping(value = { "/commandes" }, method = RequestMethod.GET)
+	public String personList() {
+		return "commandes";
+	}
 }
