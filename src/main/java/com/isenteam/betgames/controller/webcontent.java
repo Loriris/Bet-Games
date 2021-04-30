cfba03f15e3f92fee9b6284e5aabc9ec629955a5
Loriris <lorismartinez1202@gmail.com>
2021-04-30 11:28:11 +0200

    Avancement site web pages documentation

--- src/main/java/com/isenteam/betgames/controller/webcontent.java
+++ src/main/java/com/isenteam/betgames/controller/webcontent.java
@@ -1,13 +1,9 @@
 package com.isenteam.betgames.controller;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 
 @Controller
 public class webcontent {
@@ -17,8 +13,8 @@ public class webcontent {
 		return "index";
 	}
 
-	@RequestMapping(value = { "/commandes" }, method = RequestMethod.GET)
+	@RequestMapping(value = { "/documentation" }, method = RequestMethod.GET)
 	public String personList() {
-		return "commandes";
+		return "documentation";
 	}
 }
