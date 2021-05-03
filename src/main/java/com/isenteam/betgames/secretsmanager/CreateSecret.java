f20ef55fe3876dc9d28405c8ae5ab5ade9a5d767
Thomas-sch98 <schmidthomas83200@gmail.com>
2021-05-03 17:05:07 +0200

    Bot officiel + ajout class

---
+++ src/main/java/com/isenteam/betgames/secretsmanager/CreateSecret.java
@@ -0,0 +1,41 @@
+package com.isenteam.betgames.secretsmanager;
+
+import com.google.cloud.secretmanager.v1.ProjectName;
+import com.google.cloud.secretmanager.v1.Replication;
+import com.google.cloud.secretmanager.v1.Secret;
+import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
+import java.io.IOException;
+
+public class CreateSecret {
+
+  public void createSecret() throws IOException {
+    // TODO(developer): Replace these variables before running the sample.
+    String projectId = "discord-bet-298709";
+    String secretId = "projects/863757871003/secrets/BotDiscordAPI";
+    createSecret(projectId, secretId);
+  }
+
+  // Add a new version to the existing secret.
+  public void createSecret(String projectId, String secretId) throws IOException {
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests. After completing all of your requests, call
+    // the "close" method on the client to safely clean up any remaining background resources.
+    try (SecretManagerServiceClient client = SecretManagerServiceClient.create()) {
+      // Build the parent name from the project.
+      ProjectName projectName = ProjectName.of(projectId);
+
+      // Build the secret to create.
+      Secret secret =
+          Secret.newBuilder()
+              .setReplication(
+                  Replication.newBuilder()
+                      .setAutomatic(Replication.Automatic.newBuilder().build())
+                      .build())
+              .build();
+
+      // Create the secret.
+      Secret createdSecret = client.createSecret(projectName, secretId, secret);
+      System.out.printf("Created secret %s\n", createdSecret.getName());
+    }
+  }
+}
