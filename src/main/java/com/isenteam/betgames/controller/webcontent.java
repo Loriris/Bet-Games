c4900e7d8ecc0aea3396f72bd5b7a5cb43eb5e68
Loriris <lorismartinez1202@gmail.com>
2021-05-13 10:42:32 +0200

    Ajout d'une page en anglais pour le siteweb Changements images amélioration siteweb

--- src/main/java/com/isenteam/betgames/controller/webcontent.java
+++ src/main/java/com/isenteam/betgames/controller/webcontent.java
@@ -1,6 +1,7 @@
 package com.isenteam.betgames.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,13 +9,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class webcontent {
 	
-	@RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
-	public String index() {
-		return "index";
+	@RequestMapping(value = { "/", "/accueil" }, method = RequestMethod.GET)
+	public String accueil() {
+		return "accueil";
 	}
 
 	@RequestMapping(value = { "/documentation" }, method = RequestMethod.GET)
-	public String personList() {
+	public String documentation() {
 		return "documentation";
+	}
+	
+	@RequestMapping(value = { "en/home" }, method = RequestMethod.GET)
+	public String home() {
+		return "en/home";
+	}
+	
+	@RequestMapping(value = { "en/documentation" }, method = RequestMethod.GET)
+	public String endocumentation() {
+		return "en/documentation";
 	}
 }
